02214617cc234770a62a91e0814abea1911766de
JUAN CARLOS <69373493+jcduro@users.noreply.github.com>
2025-12-26 20:08:31 -0500

    Create buscador.php

---
+++ buscador.php
@@ -0,0 +1,97 @@
+ <!-- Name: JC Duro Dashboard - Header File
+     |  ___|  __ \  |   |  _ \   _ \   
+     | |      |   | |   | |   | |   |  
+ \   | |      |   | |   | __ <  |   |  
+\___/ \____| ____/ \___/ _| \_\\___/   
+                                       
+  ___|  _ \  __ \  ____|    _ )   _ _| __ \  ____|    \     ___|  
+ |     |   | |   | __|     _ \ \   |  |   | __|     _ \  \___ \  
+ |     |   | |   | |      ( `  <   |  |   | |      ___ \       | 
+\____|\___/ ____/ _____| \___/\/ ___|____/ _____|_/    _\_____/  
+
+  https://jcduro.bexartideas.com/index.php | 2026 | JC Duro Code & Ideas
+
+------------------------------------------------------------------------------- -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>JcDuroDashBoard</title>
+
+         <!-- Buscador1 Cards.css -->
+    <link rel="stylesheet" href="css/buscador1.css">
+ 
+ 
+
+
+    </head>
+
+<body>
+ 
+
+         <div class="row">
+               <div class="w-100">
+                <div class="card">
+                  <div class="card-body">
+                        <div class="header">
+                         <h1>🔍 Buscador</h1>
+                          <a href="https://github.com/jcduro" class="back-btn" target="_blank">
+                          <i class="mdi mdi-github-circle" style="font-size: 28px;"></i>
+                          </a>
+                          <a href="/proyectos/dashjc/map.php" class="back-btn">← Mapa de Visitas 🌎 </a>
+                         <a href="/proyectos/dashjc/index.php" class="back-btn">← Volver al Dashboard</a>
+                    </div>
+
+
+
+<main class="neon-search-main">
+  <section class="neon-search-wrapper">
+    <header class="neon-search-header">
+      <h4 class="neon-search-title">Buscador por país</h4>
+      <small class="neon-search-subtitle">
+        Filtra por nombre de país
+      </small>
+      <input
+        type="text"
+        id="filter"
+        class="neon-search-input"
+        placeholder="Buscar país..."
+        autocomplete="off"
+      >
+    </header>
+
+    <ul id="result" class="neon-user-list">
+      <li class="neon-user-item">
+        <h3 class="neon-user-loading">Cargando países...</h3>
+      </li>
+    </ul>
+  </section>
+</main>
+
+
+
+
+</div>
+</div>
+</div>
+</div></div>
+
+
+
+    <script src="js/buscador1.js"></script>
+    <!-- End custom js for this page -->
+
+
+
+
+
+  </body>
+</html>
+
+
+
